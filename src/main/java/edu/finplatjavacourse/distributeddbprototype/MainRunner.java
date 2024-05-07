58504d020bef63ebc07de5f0b2219b6e7b8632b0
Maksim Skobaro <mkskobaro@gmail.com>
2024-05-07 19:20:01 +0300

    Initial Commit

---
+++ src/main/java/edu/finplatjavacourse/distributeddbprototype/MainRunner.java
@@ -0,0 +1,8 @@
+package edu.finplatjavacourse.distributeddbprototype;
+
+
+public class MainRunner {
+    public static void main(String[] args) {
+        System.out.println("Hello, db");
+    }
+}
